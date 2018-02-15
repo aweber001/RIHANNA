5eb601518bbd3ef64fb50fdfc6809f5c0ed0bbc1
Unknown <spauvert@enseirb-matmeca.fr>
2018-02-15 11:39:35 +0100

    First Commit

---
+++ Main.m
@@ -0,0 +1,13 @@
+load('DataTest.mat')
+clearvars -except Antoine AxelleRR Benjamin Paul SimonRR SimonRRlay SimonRRsit SimonRRstand Youssef
+close all
+
+% RIHANNA Project : This script is used for every test. Every function can
+% be launched in this script in order to display. Please, write as many comments as possible in
+% English.
+
+% Members of the project : A.Rolin, B.Scialom, A.Weber, N.Cohen, Y.Nahim,
+% P.Burguet, S.Pauvert, all Engineer students in Telecommunications at the
+% ENSEIRB-MATMECA
+
+
