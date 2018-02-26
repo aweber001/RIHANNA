157e92e73684dca7c2719320c321d60024e2ee7f
Unknown <paul02400@hotmail.fr>
2018-02-26 15:34:43 +0100

    26/02
    MSE

---
+++ MSE/coarse_grain.m
@@ -0,0 +1,43 @@
+%%taille
+%rem pour reste de la division euclidienne et floor(a/b) pou le quotient
+%ainsi on sera sur que la taille est divisible par la scale
+%Prepartion du signal et crétaion des variables necessaire
+%ajout de copyright 
+
+
+function [cg_signal,original_signal_adapted]=coarse_grain(original_signal,scale)
+%cette fonction renvoie, pour un signal d'origine(invariant) et un scale, le signalen grain-crossier correspondant 
+% original_signal=;
+l=length(original_signal);
+% scale=;
+a=rem(length(original_signal),scale);
+%original_signal_adapted est le signal sur lequel seront fait les calculs,invariant
+%cg_signal est le signal en grain-crossier obtenu 
+
+if a==0
+   original_signal_adapted=original_signal;
+   cg_signal=zeros(1,floor(l/scale));
+else
+    original_signal_adapted=cat(2,original_signal,zeros(1,scale-a));
+    cg_signal=zeros(1,floor(l/scale)+1);
+end
+
+%%remplissage de cg
+
+for j=1:length(cg_signal)
+    for i= (j-1)*scale+1: j*scale
+        cg_signal(j)=cg_signal(j)+original_signal_adapted(i);
+    end
+    cg_signal(j)=cg_signal(j)/scale;
+end
+%%coté esthetique ' = ' , nom fonction ...
+
+
+
+
+end
+
+    
+    
+
+
