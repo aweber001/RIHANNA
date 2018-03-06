2a264199d0b3bf1b4eade303e53eb04cc7ac6111
Unknown <noemie_cohen94@hotmail.fr>
2018-03-06 14:27:38 +0100

    DFA SIGNAL INV
    En prenaant en compte la fin du signal dans le calcul de Fn

--- DFA/main_DFA_imp.m
+++ DFA/main_DFA_imp.m
@@ -2,9 +2,10 @@ clear all; close all; clc;
 load('DataTestDFA.mat');
 
 %%Initialisation des variables
-Signal=(SimonRRlay(1:298,1))';%signal de départ
+Signal=(SimonRRstand(1:297,1))';%signal de départ
+Signal=SimonRRsit';
 %scale=[16 ,32 ,64, 128, 256 ];
-scale=[4:10]
+scale=[4:10];
 L=length(scale);
 Fn=zeros(1,L);
 absis=(1:length(Signal));
@@ -12,11 +13,14 @@ order=1;%ordre pour la regression linéaire
 recouvrement=0;
 %%ETAPE 1 : ENLEVER MOYENNE
 Signal=cumsum(Signal-mean(Signal));
+Signal_inv=Signal(end:-1:1);
+absis_inv=absis(end:-1:1);
 %%ETAPE 2: 
 for kk=1:L
     N_trame=scale(kk);
     [Signal_cut,Absis_cut]=Decoupage_RI(Signal,absis,N_trame, recouvrement,'Rectangle');
-    RFN=Calcul_RFN(Signal_cut,Absis_cut,order);
+    [Signal_inv_cut,Absis_inv_cut]=Decoupage_RI(Signal_inv,absis_inv,N_trame, recouvrement,'Rectangle');
+    RFN=Calcul_RFN(Signal_cut,Absis_cut,Signal_inv_cut,Absis_inv_cut,order);
     Fn(1,kk)=sqrt(mean(RFN.^2));
 end
 
