07de398f183fe0668fa43dfdb8bc9570d87dfc0d
Benjamin <ben.scialom@gmail.com>
2018-04-08 14:32:11 +0200

    Début interface Matlab

---
+++ INTERFACE/main_interface.m
@@ -0,0 +1,138 @@
+function varargout = main_interface(varargin)
+% MAIN_INTERFACE MATLAB code for main_interface.fig
+%      MAIN_INTERFACE, by itself, creates a new MAIN_INTERFACE or raises the existing
+%      singleton*.
+%
+%      H = MAIN_INTERFACE returns the handle to a new MAIN_INTERFACE or the handle to
+%      the existing singleton*.
+%
+%      MAIN_INTERFACE('CALLBACK',hObject,eventData,handles,...) calls the local
+%      function named CALLBACK in MAIN_INTERFACE.M with the given input arguments.
+%
+%      MAIN_INTERFACE('Property','Value',...) creates a new MAIN_INTERFACE or raises the
+%      existing singleton*.  Starting from the left, property value pairs are
+%      applied to the GUI before main_interface_OpeningFcn gets called.  An
+%      unrecognized property name or invalid value makes property application
+%      stop.  All inputs are passed to main_interface_OpeningFcn via varargin.
+%
+%      *See GUI Options on GUIDE's Tools menu.  Choose "GUI allows only one
+%      instance to run (singleton)".
+%
+% See also: GUIDE, GUIDATA, GUIHANDLES
+
+% Edit the above text to modify the response to help main_interface
+
+% Last Modified by GUIDE v2.5 06-Apr-2018 12:13:30
+
+% Begin initialization code - DO NOT EDIT
+gui_Singleton = 1;
+gui_State = struct('gui_Name',       mfilename, ...
+                   'gui_Singleton',  gui_Singleton, ...
+                   'gui_OpeningFcn', @main_interface_OpeningFcn, ...
+                   'gui_OutputFcn',  @main_interface_OutputFcn, ...
+                   'gui_LayoutFcn',  [] , ...
+                   'gui_Callback',   []);
+if nargin && ischar(varargin{1})
+    gui_State.gui_Callback = str2func(varargin{1});
+end
+
+if nargout
+    [varargout{1:nargout}] = gui_mainfcn(gui_State, varargin{:});
+else
+    gui_mainfcn(gui_State, varargin{:});
+end
+% End initialization code - DO NOT EDIT
+
+
+% --- Executes just before main_interface is made visible.
+function main_interface_OpeningFcn(hObject, eventdata, handles, varargin)
+% This function has no output args, see OutputFcn.
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+% varargin   command line arguments to main_interface (see VARARGIN)
+
+% Choose default command line output for main_interface
+handles.output = hObject;
+
+% Update handles structure
+guidata(hObject, handles);
+
+% UIWAIT makes main_interface wait for user response (see UIRESUME)
+% uiwait(handles.figure1);
+
+
+% --- Outputs from this function are returned to the command line.
+function varargout = main_interface_OutputFcn(hObject, eventdata, handles) 
+% varargout  cell array for returning output args (see VARARGOUT);
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Get default command line output from handles structure
+varargout{1} = handles.output;
+
+
+% --------------------------------------------------------------------
+function Menu_Callback(hObject, eventdata, handles)
+% hObject    handle to Menu (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+
+% --------------------------------------------------------------------
+function EMD_Callback(hObject, eventdata, handles)
+% hObject    handle to EMD (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+
+% --------------------------------------------------------------------
+function DFA_Callback(hObject, eventdata, handles)
+% hObject    handle to DFA (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+
+% --------------------------------------------------------------------
+function Power_Analysis_Callback(hObject, eventdata, handles)
+% hObject    handle to Power_Analysis (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+
+% --------------------------------------------------------------------
+function MSE_Callback(hObject, eventdata, handles)
+% hObject    handle to MSE (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+
+% --------------------------------------------------------------------
+function Basic_Callback(hObject, eventdata, handles)
+% hObject    handle to Basic (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+
+% --------------------------------------------------------------------
+function Inverse_Callback(hObject, eventdata, handles)
+% hObject    handle to Inverse (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+
+% --------------------------------------------------------------------
+function Online_Callback(hObject, eventdata, handles)
+% hObject    handle to Online (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+
+% --- Executes during object creation, after setting all properties.
+function Logo_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to Logo (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+imshow('logo2.jpg');
+% Hint: place code in OpeningFcn to populate Logo
