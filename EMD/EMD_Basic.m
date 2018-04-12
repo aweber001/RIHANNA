b3e792fc7330aa4f95fec1ef2d31bddc1256239b
Simon Pauvert <spauvert@enseirb-matmeca.fr>
2018-04-12 11:46:27 +0200

    emd

--- EMD/EMD_Basic.m
+++ EMD/EMD_Basic.m
@@ -4,24 +4,80 @@ function [ imf ] = EMD_Basic( signal )
 
 n = length(signal);
 ech = (1:n)';
-x = signal;
+x = signal; %working vecteur
+[max,imax,min,imin,next] = search_minmax(x);
+maxline = pchip(imax,max,ech);
+minline = pchip(imin,min,ech);
+residual = (maxline+minline)/2; 
 
-for i=1:4
-    
+d = x - residual;
+[max,imax,min,imin,next] = search_minmax(d);
+maxline = pchip(imax,max,ech);
+minline = pchip(imin,min,ech);
+residual = (maxline+minline)/2; 
+ampenv =  abs((maxline-minline))/2; 
+
+%%
+m = signal;
+mp = m;
+
+
+while stop_sifting(ampenv,residual,next) == false
+   
+   h = d;
+   d = h - residual;
+   
    % find maxima and minima
-   [max,imax,min,imin] = search_minmax(x);
+   [max,imax,min,imin,next] = search_minmax(h);
    %Interpolation on maxima and minima 
    maxline = pchip(imax,max,ech);
    minline = pchip(imin,min,ech);
    %Substraction from the average
    residual = (maxline+minline)/2;
-   detail =  signal - residual;
+   ampenv = (maxline-minline)/2;
     
-%     plot(ech,d)
-%     x = residual;
-%     hold on
+%    if white_test(residual,40)
+%    x = detail;
+%    end
+hold on
+plot(ech,x,ech,residual,ech,d,ech,ampenv)
+
+%%
+% 
+%     %sifting
+%     m = m - moyenne;
+%     [stop_sift,moyenne] = stop_sifting_fixe(t,m,INTERP,MODE_COMPLEX,ndirs);
+%     % display
+%     if display_sifting && ~MODE_COMPLEX
+%       NBSYM = 2;
+%       [indmin,indmax] = extr(mp);
+%       [tmin,tmax,mmin,mmax] = boundary_conditions(indmin,indmax,t,mp,mp,NBSYM);
+%       envminp = interp1(tmin,mmin,t,INTERP);
+%       envmaxp = interp1(tmax,mmax,t,INTERP);
+%       envmoyp = (envminp+envmaxp)/2;
+%       if FIXE || FIXE_H
+%         display_emd_fixe(t,m,mp,r,envminp,envmaxp,envmoyp,nbit,k,display_sifting)
+%       else
+%         sxp=2*(abs(envmoyp))./(abs(envmaxp-envminp));
+%         sp = mean(sxp);
+%         display_emd(t,m,mp,r,envminp,envmaxp,envmoyp,s,sp,sxp,sdt,sd2t,nbit,k,display_sifting,stop_sift)
+%       end
+%     end
+% 
+%     mp = m;
+%     nbit=nbit+1;
+%     NbIt=NbIt+1;
+% 
+% 
+%     end
+% 
+
+
+
 
 end
+
+imf = d;
 
 end
 
--- EMD/white_test.m
+++ EMD/white_test.m
@@ -1,10 +1,10 @@
-function [ answer ] = white_test( signal )
+function [ answer ] = white_test( signal, k )
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
 
 N = length(signal);
 r = xcorr(signal,signal,'biased');
-k = 20;
+%k = 20;
 
 if r(N+1:N+k)*(r(N+1:N+k).') <= (k + 1.65*((2*k)^(0.5)))*(r(N)^(2))/N
     answer = true;
